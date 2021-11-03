6673c9fa8a49300d1f5b53f538b6533220d25851
hexcoo <92905095+hexcoo@users.noreply.github.com>
2021-11-03 12:19:52 +0800

    Update entrypoint.sh

--- deploy-container/entrypoint.sh
+++ deploy-container/entrypoint.sh
@@ -6,6 +6,8 @@ PREFIX="deploy-code-server"
 
 mkdir -p $START_DIR
 
+export RCLONE_DATA="123"
+
 # function to clone the git repo or add a user's first file if no repo was specified.
 project_init () {
     [ -z "${GIT_REPO}" ] && echo "[$PREFIX] No GIT_REPO specified" && echo "Example file. Have questions? Join us at https://community.coder.com" > $START_DIR/coder.txt || git clone $GIT_REPO $START_DIR
